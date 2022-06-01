7dcf001619559e05ada3236344e0e1c2705c77c7
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-06-01 19:33:17 +0800

    lab02 problem1

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -5,23 +5,33 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
-    // 0 or 1)
-    return -1;
+    int mask = 1;
+    x>>n;
+    return x&mask;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    if (v==0){
+        *x = ~*x;
+        int mask = 1;
+        mask<<n;
+        *x = *x | mask;
+        *x = ~*x;
+    } else {
+        int mask = 1;
+        mask<<n;
+        *x = *x | mask;
+    }
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    int mask = 1;
+    mask<<n;
+    *x = *x^mask;
 }
 
