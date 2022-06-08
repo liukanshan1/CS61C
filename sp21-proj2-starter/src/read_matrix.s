0c005113b697cba7a56f87df9ecb50afcdeb5376
LiuKanshan <1269595029@qq.com>
2022-06-08 07:05:17 -0700

    lost some code and update readmatrix

--- sp21-proj2-starter/src/read_matrix.s
+++ sp21-proj2-starter/src/read_matrix.s
@@ -25,18 +25,90 @@
 #   this function terminates the program with error code 119.
 # ==============================================================================
 read_matrix:
-
     # Prologue
-	
-
-
-
-
-
-
-
-
+	addi sp, sp, -32
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp)
+    sw s6, 24(sp)
+    sw s7, 28(sp)
+    
+    # Save
+	mv s0, a0
+    mv s1, a1
+    mv s2, a2
+    mv s3, ra
+    # Prepare Jump
+	mv a1, a0
+    mv a2, x0
+	jal fopen
+    addi t0, x0, -1
+    beq a0, t0, error2
+    mv s4, a0 # File
+    # Prepare Jump
+    addi a0, x0, 8
+    jal malloc
+    beq x0, a0 error1
+    mv s5, a0 # Row and Col
+    # Prepare Jump
+    mv a1, s4
+    mv a2, s5
+    addi a3, x0, 8
+    jal fread
+    beq a0, a3, error3
+    lw t0, 0(s5) # Row
+    lw t1, 4(s5) # Col
+    # Prepare Jump
+    mul t2, t0, t1
+    addi t3, x0, 4
+    mul s7, t2, t3 # Matrix Size
+    mv a0, s7
+    jal malloc
+    beq x0, a0 error1
+    mv s6, a0 # Matrix
+    # Prepare Jump
+    mv a1, s4
+    mv a2, s6
+    mv a3, s7
+    jal fread
+    beq a0, a3, error3
+    # Restore
+    mv a0, s0
+    mv a1, s1
+    mv a2, s2
+    mv ra, s3
+    # Prepare Return
+    mv a0, s6
+    lw t0, 0(s5) # Row
+    lw t1, 4(s5) # Col
+    sw t0, 0(a1)
+    sw t1, 0(a2)
+    
     # Epilogue
-
-
+	lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw s6, 24(sp)
+    lw s7, 28(sp)
+    addi sp, sp, 32
     ret
+
+error1:
+	addi a1, x0, 116
+    jal exit2
+error2:
+	addi a1, x0, 117
+    jal exit2
+error3:
+	addi a1, x0, 118
+    jal exit2
+error4:
+	addi a1, x0, 119
+    jal exit2
+
