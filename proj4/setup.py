76d1b91caab90b9228a22f997081db0b17496288
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-07-19 15:58:16 +0800

    upload project4

---
+++ proj4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+import sysconfig
+
+def main():
+    CFLAGS = ['-g', '-Wall', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
+    LDFLAGS = ['-fopenmp']
+    # Use the setup function we imported and set up the modules.
+    # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
+    # TODO: YOUR CODE HERE
+    raise NotImplementedError("You need to complete task 2 to install your module!")
+
+if __name__ == "__main__":
+    main()
